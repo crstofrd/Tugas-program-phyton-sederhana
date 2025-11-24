cd81f49c06ffff7197ecab6eda99ee108e7bf011
Hera28 <116450997+Hera28@users.noreply.github.com>
2025-11-25 00:29:39 +0800

    Delete input_handler.py

--- input_handler.py
+++
@@ -1,9 +0,0 @@
-#inputhandler.py 
-def get_numbers():
-    while True:
-        try:
-            a = float(input("Masukkan angka pertama: "))
-            b = float(input("Masukkan angka kedua : "))
-            return a, b
-        except ValueError:
-            print("Input tidak valid! Harus angka.\n")
