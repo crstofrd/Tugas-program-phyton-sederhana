7f242f9ef2ba00c5cf96926aa9eec81290ed6292
Hera28 <116450997+Hera28@users.noreply.github.com>
2025-11-24 23:16:53 +0800

    Update input_handler.py

--- input_handler.py
+++ input_handler.py
@@ -1,1 +1,9 @@
-operasi kalkulator di python
+#inputhandler.py 
+def get_numbers():
+    while True:
+        try:
+            a = float(input("Masukkan angka pertama: "))
+            b = float(input("Masukkan angka kedua : "))
+            return a, b
+        except ValueError:
+            print("Input tidak valid! Harus angka.\n")
