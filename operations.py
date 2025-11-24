3568478fe8860016b046454be74ec508aa2d059f
Hera28 <116450997+Hera28@users.noreply.github.com>
2025-11-24 23:13:19 +0800

    Update operations.py

--- operations.py
+++ operations.py
@@ -1,1 +1,15 @@
+#operation.py
 
+def tambah(a, b):
+    return a + b
+
+def kurang(a, b):
+    return a - b
+
+def kali(a, b):
+    return a * b
+
+def bagi(a, b):
+    if b == 0:
+        return "Error: Tidak bisa membagi dengan nol!"
+    return a / b
